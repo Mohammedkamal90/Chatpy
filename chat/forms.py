63b418edbff03e3c881cb483068d4a98fb32fc24
mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-05-27 00:39:47 +0300

    change style with tailwind

--- chat/forms.py
+++ chat/forms.py
@@ -6,3 +6,8 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}),
+        }
