26b86aac43a81d933563199c2ca06747af5e187c
mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-05-28 00:43:06 +0300

    adjust forms styling

--- chat/forms.py
+++ chat/forms.py
@@ -9,10 +9,11 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ['username', 'password1', 'password2']
         widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
-            'password1': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
-            'password2': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}),
+        'username': forms.TextInput(attrs={'class': 'block-inline mb-2 mr-20 ml-3 px-4 py-2 rounded border border-gray-300 placeholder-gray-500', 'placeholder': 'Username'}),
+        'password1': forms.PasswordInput(attrs={'class': 'block-inline mb-2 mr-25 ml-3 px-4 py-2 rounded border border-gray-300 placeholder-gray-500', 'placeholder': 'Password'}),
+        'password2': forms.PasswordInput(attrs={'class': 'block mb-4 ml-5 mr-20 px-4 py-2 rounded border border-gray-300 placeholder-gray-500', 'placeholder': 'Confirm Password', }),
         }
+
 
 class GroupForm(forms.ModelForm):
     class Meta:
