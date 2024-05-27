a10f24b351059f355d14734afc6dddf902fd6ca1
mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-05-27 03:49:25 +0300

    fixing register redirect

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import SignUpForm
 
 def index(request):
